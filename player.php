433e8665f6b86dc55dd54b7a75d6985e99981d6a
Zain Imtiaz <zenimtiazz@gmail.com>
2021-10-28 16:54:30 +0200

    step 9completed

--- player.php
+++ player.php
@@ -3,22 +3,29 @@ declare(strict_types=1);
 
 class Player{
     private array $cards = [];
-    private bool $lost = FALSE;
+    private bool $lost = false;
+    private const maxVal = 21;
 
     public function __construct(Deck $deck)
     {
      array_push($this->cards, $deck->drawCard(), $deck->drawCard());
 
     }
-public function hit(){
 
+public function hit(){
+array_push($this->cards,$deck->drawCard());
+// if($this->getScore()>21)
+//use class constant
+if($this->getScore() > self::maxVal){
+$this->lost=true;
+}
 }
 
 public function surrender(){
-    
+    $this->lost =true;
 }
 
-public function getScore(){
+public function getScore(DECK $deck){
     foreach ($this->cards as  $value) {
         $totalValue += cards->getValue();
      
